a2d4aa8c57d3ec99e5c435346d0531a2c53c207b
HeatSink Ru <samueldittedestree@gmail.com>
2021-10-15 11:53:38 +0200

    Add files via upload

---
+++ ros.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#Script d'installation de ROS 2 Galactic sur une installation Ubuntu
+#Pour lancer le script :
+#chmod +x ros.sh
+#./ros.sh
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+sudo apt update -y && sudo apt install -y curl gnupg lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt install -y \
+  build-essential \
+  cmake \
+  git \
+  python3-colcon-common-extensions \
+  python3-flake8 \
+  python3-pip \
+  python3-pytest-cov \
+  python3-rosdep \
+  python3-setuptools \
+  python3-vcstool \
+  wget
+python3 -m pip install -U \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest \
+  setuptools \
+  testresources \
+  importlib-metadata \
+  importlib-resources \
+  zipp
+mkdir -p ~/ros2_galactic/src
+cd ~/ros2_galactic
+wget https://raw.githubusercontent.com/ros2/ros2/galactic/ros2.repos
+vcs import src < ros2.repos
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-5.3.1 urdfdom_headers"
+cd ~/ros2_galactic/
+colcon build --symlink-install
+echo "source ~/ros2_galactic/install/local_setup.bash" >> ~/.bashrc
+echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+echo "export _colcon_cd_root=~/ros2_install" >> ~/.bashrc
---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Script d'installation de ROS 2 Galactic sur une installation Ubuntu
+#Pour lancer le script :
+#chmod +x ros.sh
+#./ros.sh
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt --fix-broken install
